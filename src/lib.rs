9f9b2850e5093f60b19bb0b3ccee402db827b536
Athir Nuaimi <athir@nuaimi.com>
2023-09-08 20:05:17 -0400

    can add a website to database

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use self::models::{NewWebsite, Website};
 pub mod models;
 pub mod schema;
 
@@ -12,4 +13,15 @@ pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connection to {}", database_url))
+}
+
+pub fn create_website(conn: &mut PgConnection, hostname: &String) -> Website {
+    use crate::schema::websites;
+
+    let new_website = NewWebsite{hostname};
+    diesel::insert_into(websites::table)
+        .values(&new_website)
+        .returning(Website::as_returning())
+        .get_result(conn)
+        .expect("Error saving new website")
 }
